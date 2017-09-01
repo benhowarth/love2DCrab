ae099b460a65ebaf93118e85fc9b1649e0763783
benhowarth <benhowarthmail@gmail.com>
2017-09-01 16:58:27 +0100

    0.0.4
    Crab eye stalk physics functional
    Moved arms down so as to not collide with eye stalks (must impliment
    collision filtering whatever that is)
    Added crab eye image

--- main.lua
+++ main.lua
@@ -17,6 +17,7 @@ function love.load()
   imgs.crabBod=love.graphics.newImage("img/crabBod.png")
   imgs.crabRArm=love.graphics.newImage("img/crabArm.png")
   imgs.crabLArm=love.graphics.newImage("img/crabArm2.png")
+  imgs.crabEye=love.graphics.newImage("img/crabEye.png")
 	love.physics.setMeter(64)
 	world=love.physics.newWorld(0,9.8*64,true)
 	world:setCallbacks(beginContact, endContact, preSolve, postSolve)
@@ -27,6 +28,7 @@ function love.load()
 
 	Crab=Class{
 		init=function(self,x,y)
+			--body setup
 			self.body={}
 		  self.body.b=love.physics.newBody(world,x,y,"dynamic")
 		  self.body.b:setMass(12)
@@ -35,46 +37,138 @@ function love.load()
 		  self.body.f:setRestitution(0.9)
 		  self.body.f:setUserData("crab")
 
+			--left arm setup
 		  self.arms={}
 		  self.arms.l={}
-		  self.arms.l.b=love.physics.newBody(world,x+70,y-20,"dynamic")
+		  self.arms.l.b=love.physics.newBody(world,x+80,y-20,"dynamic")
 		  self.arms.l.b:setMass(6)
 		  self.arms.l.s=love.physics.newRectangleShape(90, 25)
 		  self.arms.l.f=love.physics.newFixture(self.arms.l.b, self.arms.l.s)
 		  self.arms.l.f:setRestitution(0.4)
 		  self.arms.l.f:setUserData("crab.arms.l")
 
-		  self.arms.l.joint=love.physics.newRevoluteJoint(self.body.b,self.arms.l.b,x+50,y-20,false)
+			--left arm joint setup
+		  self.arms.l.joint=love.physics.newRevoluteJoint(self.body.b,self.arms.l.b,x+65,y-20,false)
 		  self.arms.l.joint:setLimits(-math.pi/3.3,-math.pi/4.6)
 		  self.arms.l.joint:setLimitsEnabled(true)
 
+			--right arm setup
 		  self.arms.r={}
-		  self.arms.r.b=love.physics.newBody(world,x-70,y-20,"dynamic")
+		  self.arms.r.b=love.physics.newBody(world,x-80,y-20,"dynamic")
 		  self.arms.r.b:setMass(6)
 		  self.arms.r.s=love.physics.newRectangleShape(90, 25)
 		  self.arms.r.f=love.physics.newFixture(self.arms.r.b, self.arms.r.s)
 		  self.arms.r.f:setRestitution(0.4)
 		  self.arms.r.f:setUserData("crab.arms.r")
 
-		  self.arms.r.joint=love.physics.newRevoluteJoint(self.body.b,self.arms.r.b,x-50,y-20,false)
+			--right arm joint setup
+		  self.arms.r.joint=love.physics.newRevoluteJoint(self.body.b,self.arms.r.b,x-65,y-20,false)
 		  self.arms.r.joint:setLimits(math.pi/4.6,math.pi/3.3)
 		  self.arms.r.joint:setLimitsEnabled(true)
+
+			--left eye setup
+			self.eyes={}
+			self.eyes.l={}
+			--left eye base stalk setup
+			self.eyes.l.stalk1={}
+			self.eyes.l.stalk1.b=love.physics.newBody(world,x+25,y-30,"dynamic")
+		  self.eyes.l.stalk1.b:setMass(2)
+		  self.eyes.l.stalk1.s=love.physics.newRectangleShape(10, 30)
+		  self.eyes.l.stalk1.f=love.physics.newFixture(self.eyes.l.stalk1.b, self.eyes.l.stalk1.s)
+		  self.eyes.l.stalk1.f:setUserData("crab.eyes.l.stalk1")
+			--left eye base stalk joint setup
+		  self.eyes.l.stalk1.joint=love.physics.newRevoluteJoint(self.body.b,self.eyes.l.stalk1.b,x+25,y-25,false)
+		  self.eyes.l.stalk1.joint:setLimits(-0.2,0.2)
+		  self.eyes.l.stalk1.joint:setLimitsEnabled(true)
+			--left eye top stalk setup
+			self.eyes.l.stalk2={}
+			self.eyes.l.stalk2.b=love.physics.newBody(world,x+25,y-50,"dynamic")
+			self.eyes.l.stalk2.b:setMass(2)
+			self.eyes.l.stalk2.s=love.physics.newRectangleShape(10, 25)
+			self.eyes.l.stalk2.f=love.physics.newFixture(self.eyes.l.stalk2.b, self.eyes.l.stalk2.s)
+			self.eyes.l.stalk2.f:setUserData("crab.eyes.l.stalk2")
+			--left eye top stalk joint setup
+			self.eyes.l.stalk2.joint=love.physics.newRevoluteJoint(self.eyes.l.stalk1.b,self.eyes.l.stalk2.b,x+25,y-45,false)
+			self.eyes.l.stalk2.joint:setLimits(-0.3,0.3)
+			self.eyes.l.stalk2.joint:setLimitsEnabled(true)
+			--left eye ball setup
+			self.eyes.l.ball={}
+			self.eyes.l.ball.b=love.physics.newBody(world,x+25,y-65,"dynamic")
+			self.eyes.l.ball.b:setMass(4)
+			self.eyes.l.ball.s=love.physics.newRectangleShape(20, 20)
+			self.eyes.l.ball.f=love.physics.newFixture(self.eyes.l.ball.b, self.eyes.l.ball.s)
+			self.eyes.l.ball.f:setUserData("crab.eyes.l.ball")
+			--left eye ball joint setup
+			self.eyes.l.ball.joint=love.physics.newRevoluteJoint(self.eyes.l.stalk2.b,self.eyes.l.ball.b,x+25,y-65,false)
+
+
+
+			--right eye setup
+			self.eyes.r={}
+			--right eye base stalk setup
+			self.eyes.r.stalk1={}
+			self.eyes.r.stalk1.b=love.physics.newBody(world,x-25,y-30,"dynamic")
+		  self.eyes.r.stalk1.b:setMass(2)
+		  self.eyes.r.stalk1.s=love.physics.newRectangleShape(10, 30)
+		  self.eyes.r.stalk1.f=love.physics.newFixture(self.eyes.r.stalk1.b, self.eyes.r.stalk1.s)
+		  self.eyes.r.stalk1.f:setUserData("crab.eyes.r.stalk1")
+			--right eye base stalk joint setup
+		  self.eyes.r.stalk1.joint=love.physics.newRevoluteJoint(self.body.b,self.eyes.r.stalk1.b,x-25,y-25,false)
+		  self.eyes.r.stalk1.joint:setLimits(-0.2,0.2)
+		  self.eyes.r.stalk1.joint:setLimitsEnabled(true)
+			--right eye top stalk setup
+			self.eyes.r.stalk2={}
+			self.eyes.r.stalk2.b=love.physics.newBody(world,x-25,y-50,"dynamic")
+			self.eyes.r.stalk2.b:setMass(2)
+			self.eyes.r.stalk2.s=love.physics.newRectangleShape(10, 25)
+			self.eyes.r.stalk2.f=love.physics.newFixture(self.eyes.r.stalk2.b, self.eyes.r.stalk2.s)
+			self.eyes.r.stalk2.f:setUserData("crab.eyes.r.stalk2")
+			--left eye top stalk joint setup
+			self.eyes.r.stalk2.joint=love.physics.newRevoluteJoint(self.eyes.r.stalk1.b,self.eyes.r.stalk2.b,x-25,y-45,false)
+			self.eyes.r.stalk2.joint:setLimits(-0.3,0.3)
+			self.eyes.r.stalk2.joint:setLimitsEnabled(true)
+			--left eye ball setup
+			self.eyes.r.ball={}
+			self.eyes.r.ball.b=love.physics.newBody(world,x-25,y-65,"dynamic")
+			self.eyes.r.ball.b:setMass(4)
+			self.eyes.r.ball.s=love.physics.newRectangleShape(20, 20)
+			self.eyes.r.ball.f=love.physics.newFixture(self.eyes.r.ball.b, self.eyes.r.ball.s)
+			self.eyes.r.ball.f:setUserData("crab.eyes.r.ball")
+			--left eye ball joint setup
+			self.eyes.r.ball.joint=love.physics.newRevoluteJoint(self.eyes.r.stalk2.b,self.eyes.r.ball.b,x-25,y-65,false)
+
 		end;
 		grounded=false;
 		draw=function(self)
 			  love.graphics.draw(imgs.crabLArm,self.arms.l.b:getX(),self.arms.l.b:getY(),self.arms.l.b:getAngle(),1,1,imgs.crabLArm:getWidth()/2,imgs.crabLArm:getHeight()/2)
 			  love.graphics.draw(imgs.crabRArm,self.arms.r.b:getX(),self.arms.r.b:getY(),self.arms.r.b:getAngle(),1,1,imgs.crabRArm:getWidth()/2,imgs.crabRArm:getHeight()/2)
 			  love.graphics.draw(imgs.crabBod,self.body.b:getX(),self.body.b:getY(),self.body.b:getAngle(),1,1,imgs.crabBod:getWidth()/2,imgs.crabBod:getHeight()/2)
+
+			  love.graphics.draw(imgs.crabEye,self.eyes.l.ball.b:getX(),self.eyes.l.ball.b:getY(),self.eyes.l.ball.b:getAngle(),1,1,imgs.crabEye:getWidth()/2,imgs.crabEye:getHeight()/2)
+
+
+			  love.graphics.draw(imgs.crabEye,self.eyes.r.ball.b:getX(),self.eyes.r.ball.b:getY(),self.eyes.r.ball.b:getAngle(),1,1,imgs.crabEye:getWidth()/2,imgs.crabEye:getHeight()/2)
 				if(hitboxes)then
+					--draw bod
+					love.graphics.polygon("line",self.body.b:getWorldPoints(self.body.s:getPoints()))
+					--draw arms
 					love.graphics.polygon("line",self.arms.l.b:getWorldPoints(self.arms.l.s:getPoints()))
 					love.graphics.polygon("line",self.arms.r.b:getWorldPoints(self.arms.r.s:getPoints()))
-					love.graphics.polygon("line",self.body.b:getWorldPoints(self.body.s:getPoints()))
+					--draw eyes
+					love.graphics.polygon("line",self.eyes.l.stalk1.b:getWorldPoints(self.eyes.l.stalk1.s:getPoints()))
+					love.graphics.polygon("line",self.eyes.l.stalk2.b:getWorldPoints(self.eyes.l.stalk2.s:getPoints()))
+					love.graphics.polygon("line",self.eyes.l.ball.b:getWorldPoints(self.eyes.l.ball.s:getPoints()))
+
+
+					love.graphics.polygon("line",self.eyes.r.stalk1.b:getWorldPoints(self.eyes.r.stalk1.s:getPoints()))
+					love.graphics.polygon("line",self.eyes.r.stalk2.b:getWorldPoints(self.eyes.r.stalk2.s:getPoints()))
+					love.graphics.polygon("line",self.eyes.r.ball.b:getWorldPoints(self.eyes.r.ball.s:getPoints()))
 				end
 		end;
 	}
 
 	crab=Crab(400,200)
-	crab2=Crab(400,0)
+	--crab2=Crab(400,0)
 
 
 
@@ -113,7 +207,7 @@ end
 
 function love.draw()
 	crab:draw()
-	crab2:draw()
+	--crab2:draw()
   love.graphics.polygon("line",ground.b:getWorldPoints(ground.s:getPoints()))
   love.graphics.print(text,10,10)
 end
